20d7d6983741898210f657f0825d4f9a24ce6b95
Cleiton Lok <cleiton.biou@gmail.com>
2020-12-13 15:59:31 -0300

    :sparkles: new pages
    Signed-off-by: Cleiton Lok <cleiton.biou@gmail.com>

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-import Rengato from './rengato.png'
+import { Link } from 'react-router-dom';
+import { Badge } from 'react-bootstrap';
 
+import Rengato from './rengato.png'
 import './index.css';
 
 export default function Home() {
@@ -11,7 +12,7 @@ export default function Home() {
             <nav id="navbarmy">
                 <a className="navbar-brand" href="/">
                     <img src={Rengato} width="30" height="30" class="d-inline-block align-top" alt=""/>
-                    TRANSFRETE
+                    Melhor Envio
                 </a>
     
                 
@@ -24,13 +25,99 @@ export default function Home() {
                             <a className="nav-link" href="/">Transportadoras</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">Cadastre sua transportadora</a>
+                            <a className="nav-link" href="/transportadora">Cadastre sua transportadora</a>
                         </li>
                     </ul>
                 </div>
             </nav>
-  
 
+            <div className="init">
+                <h1>ECONOMIZE NA GESTÃO DE FRETES</h1>
+                <p>Cote simultaneamente com diversas transportadoras e gere envios com rastreio inteligente em uma plataforma integrada</p>
+                <br></br>
+                <Link to="#">DÊ O PRIMEIRO PASSO</Link>
+            </div>
+
+            <div className="form-carga">
+                <h2>Dados da carga</h2>
+                <form action="">
+                    <div className="form-group inline">
+                        <input className="form-control" type="text" placeholder="CEP Origem"/>
+                        <label htmlFor=""></label>
+                        <input className="form-control" type="text" placeholder="CEP Destino"/>
+                    </div>
+                    <div className="form-group inline">
+                        <label htmlFor="">Altura (cm)</label>
+                        <input className="form-control" type="text"/>
+                        <label htmlFor="">Largura (cm)</label>
+                        <input className="form-control" type="text"/>
+                        <label htmlFor="">Comprimento (cm)</label>
+                        <input className="form-control" type="text"/>
+                        <label htmlFor="">Volumes (UN)</label>
+                        <input className="form-control" type="text"/>
+                    </div>
+                    <div className="form-group inline">
+                        <label htmlFor="">Valor (R$)</label>
+                        <input type="text" className="form-control"/>
+
+                        <label htmlFor="">Peso (KG)</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="center-align">
+                        <button type="submit" className="btn btn-primary">COTAR FRETE AGORA!</button>
+                    </div>
+                </form>
+            </div>
+        
+            <div className="transportadora">
+                <h2>Transportadoras</h2>
+                <div className="row">
+                    <div className="col">
+                        <div className="cidades">
+                            <a href="#" className="collection-item">
+                                <span className="badge">{'649 '} transportadoras</span>
+                                São Paulo
+                            </a>
+                            <a href="#" className="collection-item">
+                                <span className="badge">{'649 '} transportadoras</span>
+                                Pernambuco
+                            </a>
+                            <a href="#" className="collection-item">
+                                <span className="badge">{'649 '} transportadoras</span>
+                                Minas Gerais
+                            </a>
+                            <a href="#" className="collection-item">
+                                <span className="badge">{'649 '} transportadoras</span>
+                                Bahia
+                            </a>
+                        </div>
+                        
+                    </div>
+
+                    <div className="col">
+                        <div className="cidades">
+                            <a href="#" className="collection-item">
+                                <span className="badge">{'649 '} transportadoras</span>
+                                Rio de Janeiro
+                            </a>
+                            <a href="#" className="collection-item">
+                                <span className="badge">{'649 '} transportadoras</span>
+                                Paraná
+                            </a>
+                            <a href="#" className="collection-item">
+                                <span className="badge">{'649 '} transportadoras</span>
+                                Rio Grande do Sul
+                            </a>
+                            <a href="#" className="collection-item">
+                                
+                                Outros...
+                            </a>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/pages/Transregi/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './index.css';
+
+export default function Transportadora() {
+    return (
+        <div className="form">
+
+        </div>
+    )
+}
